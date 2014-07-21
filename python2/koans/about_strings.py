02fa6ddff8b0900beebc5a040c1b54a317986c4f
vml-akoebbe <akoebbe@vml.com>
2014-07-20 23:00:28 -0500

    Koan 13 & 14 Complete

--- python2/koans/about_strings.py
+++ python2/koans/about_strings.py
@@ -28,11 +28,11 @@ class AboutStrings(Koan):
 
     def test_use_single_quotes_to_create_string_with_double_quotes(self):
         string = 'He said, "Go Away."'
-        self.assertEqual(__, string)
+        self.assertEqual('He said, "Go Away."', string)
 
     def test_use_double_quotes_to_create_strings_with_single_quotes(self):
         string = "Don't"
-        self.assertEqual(__, string)
+        self.assertEqual("Don't", string)
 
     def test_use_backslash_for_escaping_quotes_in_strings(self):
         a = "He said, \"Don't\""
