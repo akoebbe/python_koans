85d3e431ba3690114a1a7d321814434d7c1a4e79
vml-akoebbe <akoebbe@vml.com>
2014-07-29 22:17:48 -0500

    Koan 87-88 Complete

--- python2/koans/about_control_statements.py
+++ python2/koans/about_control_statements.py
@@ -11,13 +11,13 @@ class AboutControlStatements(Koan):
             result = 'true value'
         else:
             result = 'false value'
-        self.assertEqual(__, result)
+        self.assertEqual('true', result)
 
     def test_if_then_statements(self):
         result = 'default value'
         if True:
             result = 'true value'
-        self.assertEqual(__, result)
+        self.assertEqual('true value', result)
         
     def test_if_then_elif_else_statements(self):
         if False:
@@ -26,7 +26,7 @@ class AboutControlStatements(Koan):
             result = 'true value'
         else:
             result = 'default value'
-        self.assertEqual(__, result)
+        self.assertEqual('true value', result)
 
     def test_while_statement(self):
         i = 1
